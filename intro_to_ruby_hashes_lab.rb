e70eb4e5ffa8a8d6c642f0d6049f78ff43daea29
Kayla Petersen <petersen1994@gmail.com>
2019-08-16 18:34:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,9 +43,9 @@ def monopoly_with_fourth_tier
 	      four_pieces_owned: 200
 	    },
 	    names: { 
-	      reading_railroad: {
+	      reading_railroad: [
 	        mortgage_value: "$100" 
-	      }, 
+	      ], 
 	      pennsylvania_railroad: {
 	        mortgage_value: "$200"
 	      },
